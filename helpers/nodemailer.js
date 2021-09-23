2612273a16d26188aad4f8bd84074eec035f4702
Handson Panggabean <hansenpanggabean8@gmail.com>
2021-09-23 07:09:50 +0700

    nodemailer worked and socketio worked but still not saved if page refreshed

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -1,8 +1,9 @@
 const nodemailer = require("nodemailer");
 
-async function main(user, name, city) {
+async function main(to, username, doctor) {
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
+    service: "gmail",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -12,23 +13,30 @@ async function main(user, name, city) {
   });
   let info = await transporter.sendMail({
     from: "handsonpanggabean2@gmail.com", // sender address
-    to: `${user}`, // list of receivers
-    subject: "Hello ✔", // Subject line
+    to: `${to}`, // list of receivers
+    subject: "Hello, I'm Handson Panggabean the admin of Halo - Dok ✔", // Subject line
     text: `
-    This is your Travel detail that you've checked out
-    Destination : ${name}
-    City        : ${city}
+    This Message is Just For Notification/Remainder For you about 5-10 minutes later you can talk with doctor ${doctor}, Now you can click ChatBox Menu, and please wait the doctor to come join into your chat room.. Thank You for parcitipation with us, Stay Healthy!
+    You : ${username}
+    Doctor: ${doctor}
     `, // plain text body
-    html: `<b>
-    This is your Travel detail that you've checked out
-    Destination : ${name}
-    City        : ${city}
-    </b>`, // html body
+    html: `<div>
+    <b>This Message is Just For Notification/Remainder</b>
+    <br><br>
+    About 5-10 minutes later you can talk with doctor ${doctor}, Now you can click ChatBox Menu, and please wait the doctor to come join into your chat room..
+    <br><br>
+    Thank You for parcitipation with us, Stay Healthy!
+    <br><br>
+    You : ${username}
+    <br>
+    Doctor: ${doctor}
+    </div>`, // html body
   });
-  console.log("Message sent: %s", info.messageId);  
+  // console.log(info);
+  console.log("Message sent: %s", info.accepted[0]);
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
 
-main().catch(console.error);
+// main().catch(console.error);
 
 module.exports = main
